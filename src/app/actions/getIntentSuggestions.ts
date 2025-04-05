ffc539c8c0ea7e1b6801bcf15d1c2de402c0fc65
Andy Ferrie <andy.ferrie@gmail.com>
2025-04-05 19:40:43 +0100

    Add getIntentSuggestions server action

---
+++ src/app/actions/getIntentSuggestions.ts
@@ -0,0 +1,14 @@
+"use server"
+
+import getTopSuggestions from "@/utils/intentSuggestions"
+import rawData from "@/data/technical_assessment_chatbot_data.json"
+import { IntentRecord } from "@/types/intents"
+
+// For this POC, we cast the mock JSON to IntentRecord[] to keep things simple.
+// Since JSON has no concept of TypeScript, it can't guarantee that `intRec` matches our `IntentKey` union.
+// In production, we'd use runtime validation (e.g. Zod) to enforce this safely.
+const data = rawData as IntentRecord[]
+
+export async function getIntentSuggestions() {
+    return getTopSuggestions(data)
+}
